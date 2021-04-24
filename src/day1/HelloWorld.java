6c2a4401d97a273149b4df4d1a0221919b4651ff
Aytaj Kareem <kareemaytaj@gmail.com>
2021-04-24 16:53:17 -0400

    Initial commit

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -3,5 +3,7 @@ package day1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Adding Github now");
+        System.out.println("YAY, THE WHOLE STUFF IN A GITHUB!");
     }
 }
